1a75f6009c24b109446db12940ce6100c380c354
jaimeadf <jaimeadanielfilho@hotmail.com>
2020-11-11 22:28:10 -0300

    Use vRP.getItemDefinition instead of separate function for each item property

--- server/managers/ItemManager.lua
+++ server/managers/ItemManager.lua
@@ -3,11 +3,12 @@ local class = require('common/utils/class')
 local ItemManager = class()
 
 function ItemManager:create(id_name, amount)
+    local name, weight, description = vRP.getItemDefinition(id_name)
     return {
         idName = id_name,
-        name = vRP.getItemName(id_name),
-        weight = vRP.getItemWeight(id_name),
-        description = vRP.getItemDescription(id_name),
+        name = name,
+        weight = weight,
+        description = description,
         amount = amount
     }
 end
