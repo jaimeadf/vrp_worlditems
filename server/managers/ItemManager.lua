59ca07011f82e7fca8bb532d20783d0f46674273
jaimeadf <jaimeadanielfilho@hotmail.com>
2020-11-11 23:14:23 -0300

    Add default values to item properties

--- server/managers/ItemManager.lua
+++ server/managers/ItemManager.lua
@@ -5,10 +5,10 @@ local ItemManager = class()
 function ItemManager:create(id_name, amount)
     local name, weight, description = vRP.getItemDefinition(id_name)
     return {
-        idName = id_name,
-        name = name,
-        weight = weight,
-        description = description,
+        idName = id_name or 'unknown',
+        name = name or id_name or 'unknown name',
+        weight = weight or 0,
+        description = description or '',
         amount = amount
     }
 end
